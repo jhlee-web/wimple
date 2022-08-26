0bfe16b38241883e53612bd3efe7ce7b6a057d45
Jihyeon lee <l_jh@seoreu.com>
2022-08-26 11:51:23 +0900

    20220826 solution, src, etc

--- js/header.js
+++ js/header.js
@@ -3,6 +3,8 @@ $(function(){
     createScrollTop()
 })
 
+let headerHeight = $('.header').height();
+
 $(window).on('scroll', function(event){
     createScrollTop()
 
@@ -27,15 +29,55 @@ function createScrollTop(){
     }
 }
 
-
-
+// 이겆 ㅓㅇ리하고 js 하나없애고싶은데.. .머리가 안돌아간다...
 function getScrollTop(){
     let scrollTop = document.documentElement.scrollTop;
-    let headerHeight = $('.header').height();
+    let url = $(location).attr('pathname')
 
-    if(scrollTop>0 || scrollTop>=headerHeight){
-        $('.header').addClass('scrolled').addClass('white')
+    // INDEX (첫페이지)일 경우
+    if(url == "/" || url == "/index.html"){
+        $('.header').removeClass('sub').addClass('white');
     }
+    // 서브페이지 (2DEPTH~) 일 경우
+    else{
+        $('.header').addClass('sub');
+
+        // 서브 페이지 배경화면 영역으로 들어간 경우
+        if(scrollTop>=headerHeight){
+            $('.header').addClass('scrolled').addClass('white')
+
+            let subHeaderBg = $('.sub-header__bg');
+           
+            // 서브페이지 배경 이미지가 있는 경우
+            if(subHeaderBg.length>0){
+                changeHeaderImg(subHeaderBg)
+            }
+        }
+    }
+    
 }
 
+function changeHeaderImg(subHeaderBg){
+    let subNav = $('.sub-header__tab-wrap')
+    // 스크롤탑이 헤더 높이보다 내려갔으면 (영상/이미지 구간이면) 로고를 흰색으로 변경
+    if ($(window).scrollTop() >= headerHeight) {
+        $('.header').addClass('white');
+        $('.header__logo-img').attr('src','./img/logo-white.png');
+
+        // 이미지(영상) 구간을 넘긴 경우 로고를 블랙(기본) 변경 + 서브 페이지 내비게이션 고정
+        if ( $(window).scrollTop() > subHeaderBg.height() ){
+            $('.header').removeClass('white');
+            $('.header__logo-img').attr('src','/img/logo.png');
+            subNav.addClass('fixed').addClass('scrolled')
+        }
+        else{
+            subNav.removeClass('fixed').addClass('scrolled')
+        }
+    }
+    // 스크롤탑이 헤더 높이보다 높음
+    else{
+        $('.header').removeClass('white');
+        $('.header__logo-img').attr('src','/img/logo.png')
+    }
+}
 
